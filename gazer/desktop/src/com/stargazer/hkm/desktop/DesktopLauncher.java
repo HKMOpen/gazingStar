8548520452b0fac96da974d8cc9519868c6c80d5
世外桃源 <jobhesk@gmail.com>
2015-10-13 10:07:53 +0800

    gds library

---
+++ gazer/desktop/src/com/stargazer/hkm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stargazer.hkm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stargazer.hkm.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
